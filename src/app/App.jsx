07f2ffa43fe4a6d658f792b9c00c55eaae1727e5
elifyilbasi <elifyilbasi@gmail.com>
2020-09-02 07:09:17 +0300

    Restructure App files

---
+++ src/app/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoPageContainer from "../todos/TodoPageContainer/TodoPageContainer";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <TodoPageContainer />
+    </div>
+  );
+}
+
+export default App;
