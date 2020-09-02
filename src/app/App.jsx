d6b39ea542833266e246d9c04ed48e4f5987e3e8
elifyilbasi <elifyilbasi@gmail.com>
2020-09-02 07:10:08 +0300

    Add initial redux store

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,17 +2,21 @@ import React from "react";
 import TodoPageContainer from "../todos/TodoPageContainer/TodoPageContainer";
 import FadeLoader from "react-spinners/FadeLoader";
 import "./App.css";
+import { Provider } from "react-redux";
+import reduxStore from "./reduxStore";
 
 function App() {
   return (
-    <div className="container">
-      <TodoPageContainer />
-      <div className="loader-container">
-        <div className="loader-wrapper">
-          <FadeLoader style={{top: "50%"}}/>
+    <Provider store={reduxStore}>
+      <div className="container">
+        <TodoPageContainer />
+        <div className="loader-container">
+          <div className="loader-wrapper">
+            <FadeLoader/>
+          </div>
         </div>
       </div>
-    </div>
+    </Provider>
   );
 }
 
