782f33d47925b51af3b433ae65bbeb19c3105c3d
elifyilbasi <elifyilbasi@gmail.com>
2020-09-02 07:10:01 +0300

    Add loading indicator for requests

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import TodoPageContainer from "../todos/TodoPageContainer/TodoPageContainer";
+import FadeLoader from "react-spinners/FadeLoader";
 import "./App.css";
 
 function App() {
   return (
     <div className="container">
       <TodoPageContainer />
+      <div className="loader-container">
+        <div className="loader-wrapper">
+          <FadeLoader style={{top: "50%"}}/>
+        </div>
+      </div>
     </div>
   );
 }
